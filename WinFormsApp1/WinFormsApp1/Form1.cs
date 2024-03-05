09b59ececc0daaa07004cacd827250a1dc188be5
nhuquynh <toannguyen.programmer@gmail.com>
2024-03-05 14:26:14 +0700

    commit

--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -6,5 +6,20 @@ namespace WinFormsApp1
         {
             InitializeComponent();
         }
+
+        private void btnDangNhap_Click(object sender, EventArgs e)
+        {
+            bool kq = checkLogin(txtTenDangNhap.Text, txtMatKhau.Text);
+
+            if(kq)
+            {
+                MessageBox.Show("Thành công");
+            }
+        }
+
+        bool checkLogin(string userName, string passWord) {
+            //read db
+            return true;
+        }
     }
 }
