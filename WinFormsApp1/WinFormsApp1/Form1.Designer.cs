83968d9df59df18b46cc7511401c633fee08a10a
TrangHuynh12 <Huynhthithuytrang.it@gmail.com>
2024-03-05 14:08:56 +0700

    commit

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,12 +28,66 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            btnDangNhap = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(126, 100);
+            label1.Name = "label1";
+            label1.Size = new Size(50, 20);
+            label1.TabIndex = 0;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(121, 164);
+            label2.Name = "label2";
+            label2.Size = new Size(50, 20);
+            label2.TabIndex = 1;
+            label2.Text = "label2";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(235, 95);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(353, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // btnDangNhap
+            // 
+            btnDangNhap.Location = new Point(235, 235);
+            btnDangNhap.Name = "btnDangNhap";
+            btnDangNhap.Size = new Size(94, 29);
+            btnDangNhap.TabIndex = 3;
+            btnDangNhap.Text = "Dang Nhap";
+            btnDangNhap.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnDangNhap);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private Button btnDangNhap;
     }
 }
